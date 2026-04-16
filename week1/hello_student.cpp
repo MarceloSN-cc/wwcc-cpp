d565db627d5296cedb16205211effe0c9a6977e0
marcelo <2marcelos004@gmail.com>
2026-04-15 23:46:13 -0700

    adding hello_student program

---
+++ week1/hello_student.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string name = "Eve";
+    string password = "Password1"
+
+    cout << "Enter your name: \n";
+    cout << name << " Hello!" <<"Welcome to C++! \n";
+    cout << "Please enter your password: " << password << endl;
+    cout << name << ", should you ever share your password with a stranger? \n"
+    << "No! It would be like exposing yourself nude, and you don't want that! " << password << endl;
+
+    return 0;
+
+
+}
